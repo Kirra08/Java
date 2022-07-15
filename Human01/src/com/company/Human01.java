4d434775fa0101ef9489bee05eff4a8c91cddc69
omumaru <99459326+omumaru@users.noreply.github.com>
2022-07-15 13:38:17 +0900

    class

---
+++ Human01/src/com/company/Human01.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Human01 {
+    public String name;
+    public int age;
+
+    public Human01() {
+        age = 20;
+        name = "山田";
+    }
+
+    public Human01(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
