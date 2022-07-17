37f1edc1576ab304d2f0b7151de2c932ad2ef988
omumaru <99459326+omumaru@users.noreply.github.com>
2022-07-17 22:34:37 +0900

    微修正

--- Object Oriented/src/Q02/Main.java
+++ Object Oriented/src/Q02/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Shop s2 = new Shop();
         s1.name = "パン屋";
         s1.sale = 100000;
-        s2.name = "洋服や";
+        s2.name = "洋服屋";
         s2.sale = 150000;
         System.out.println(s1.name + "売上: " + s1.sale);
         System.out.println(s2.name + "売上: " + s2.sale);
